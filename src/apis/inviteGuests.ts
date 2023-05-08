ee5b8054ad2f378f2d88e5d8567146fb9aa39965
Alexander Mendivil <alexanderima@outlook.es>
2023-05-07 21:41:25 -0700

    fix post payload

--- src/apis/inviteGuests.ts
+++ src/apis/inviteGuests.ts
@@ -3,8 +3,6 @@ import axios from "axios"
 
 export const inviteGuests = async (ids: GridRowSelectionModel) =>{
     const token = localStorage.getItem( 'jwt_bride')
-    const data = await axios.post(' http://localhost:3001/invite', {
-        data: ids, 
-        headers: {'Authorization': `Bearer ${token}`}} )
+    const data = await axios.post(' http://localhost:3001/invite', { ids },{ headers: {'Authorization': `Bearer ${token}`}})
     return data.data
 }
